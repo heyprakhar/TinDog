9f7052e759212056dc208e98ca7536c714b51f48
Prakhar Tripathi <talk2prakhartripathi@gmail.com>
2024-05-15 18:24:47 +0530

    update tindog

---
+++ TinDog/script.js
@@ -0,0 +1,6 @@
+alert("You are about to enter the tinder of dogs");
+function toggleNewsLetter(){
+    const newsLetter = document.getElementById("newsLetter");
+
+    newsLetter.classList.toggle("active");
+}
